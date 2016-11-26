f0e401e84b0fdd416413a0f749af148cc5548b8a
Steve Baldwin <stevebaldwin21@googlemail.com>
2016-11-26 00:23:38 +0000

    Update assert.php

--- tests/assert.php
+++ tests/assert.php
@@ -1,14 +1,5 @@
 <?php
 
-use Windows\Diagnostics\EventLog;
-use Windows\Diagnostics\EventType;
-
-EventLog::setSource("PHP");
-
-if (EventLog::writeEntry("Hello World", EventType::Warning)) {
-	return true;
-} else {
-	return false;
-}
+//excluded.
 
 ?>
